f15b3ab9b758b1f42a7578f7303713e475225d04
jerciampi <jer.ciampi@gmail.com>
2026-04-01 16:18:38 +0200

    clean up github et ajout base de données

--- web/backend/modifier_badge_form.php
+++ web/backend/modifier_badge_form.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <title>Backend : Modifier une balade - formulaire</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="">
 </head>
 
 
