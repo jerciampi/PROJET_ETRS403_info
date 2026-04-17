1753006376c14e128bfae4f4d72562ac106842ce
jerciampi <jer.ciampi@gmail.com>
2026-04-17 08:42:40 +0200

    changement ajouter badge

--- web/backend/ajouter_badge.php
+++ web/backend/ajouter_badge.php
@@ -16,7 +16,7 @@
 <body>
 <?php 
 
-$code_badge = $_POST['code_badge'];
+$codebadge = $_POST['code_badge'];
 $nom_badge = $_POST['nom_badge'];
 if (isset($_POST['admin'])) {
   $admin = (bool) $_POST['admin'];
@@ -25,14 +25,27 @@ if (isset($_POST['admin'])) {
 }
 $dbh = new PDO('mysql:dbname=test;host=localhost;charset=utf8', 'root', '');
 
+$result = $dbh->query("SELECT * FROM codebadge1");
 
+      $danslaBase = false;
 
-$result = $dbh->query("INSERT INTO codebadge1(code_badge, nom_badge, admin) 
-VALUES ('$code_badge', '$nom_badge', '$admin')");
+      while (($row = $result->fetch(PDO::FETCH_ASSOC))&& $danslaBase == False) {
+        if ($row['code_badge'] == $codebadge) {
+          $danslaBase = true;
+          echo "<p>\n";
+        echo "Le badge est déjà dans la base   <br>\n";
+        echo '</p>';
+          }
+        }
 
-echo "<p>\n";
-echo "Le badge a bien été rajouté dans la base  : <br>\n";
-echo '</p>'
+      if ($danslaBase == false) {
+        $res = $dbh->query("INSERT INTO codebadge1(code_badge, nom_badge, admin) 
+VALUES ('$codebadge', '$nom_badge', '$admin')");
+        echo "<p>\n";
+        echo "Le badge a bien été rajouté dans la base   <br>\n";
+        echo '</p>';
+      }
+
 ?>
 
 </body>
